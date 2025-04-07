71643eda51a1c0550c25ff75ab055ad9c202ca68
Yuldashev <ikrom921@gmail.com>
2025-04-07 11:57:47 +0500

    Initial Commit

---
+++ BalancePro/BalanceProApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceProApp.swift
+//  BalancePro
+//
+//  Created by Yuldashev Fazliddin on 07/04/25.
+//
+
+import SwiftUI
+
+@main
+struct BalanceProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
